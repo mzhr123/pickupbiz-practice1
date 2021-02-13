e97ab6aad31641be7af69761a2fb11fc53a5f335
mzhr123 <76685693+mzhr123@users.noreply.github.com>
2021-02-13 16:10:24 +0530

    imposrt

--- src/components/fetchingDataFrmJSON/Datachild.jsx
+++ src/components/fetchingDataFrmJSON/Datachild.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const DataChild =()=>{
     return(
         <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime adipisci officiis asperiores libero, iure alias enim amet, minima exercitationem, harum corrupti nulla sunt voluptates recusandae inventore voluptate blanditiis. Esse, delectus?</p>
+            <p>The Student name is, his Age is and he is in Standard </p>
         </div>
     );
 };
