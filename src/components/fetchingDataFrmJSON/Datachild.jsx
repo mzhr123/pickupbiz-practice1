0750c7fb67233b77cd19d02a603b0d7750f5bed6
mzhr123 <76685693+mzhr123@users.noreply.github.com>
2021-02-13 16:30:33 +0530

    import

--- src/components/fetchingDataFrmJSON/Datachild.jsx
+++ src/components/fetchingDataFrmJSON/Datachild.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const DataChild =()=>{
     return(
         <div>
-            <img src="./Assets/image1" />
+            <img src="./Assets/image1.jpg" width="100" height="100" />
             <p>The Student name is, his Age is and he is in Standard </p>
         </div>
     );
