34d9b8b88ad3f5336158939040c1f1a634acc656
mzhr123 <76685693+mzhr123@users.noreply.github.com>
2021-02-13 15:45:37 +0530

    jason

--- src/components/State/Reducer/personDetailsReducer.js
+++ src/components/State/Reducer/personDetailsReducer.js
@@ -1,12 +1,12 @@
 const personDetailsReducer ={
     personDetails:[
-        {name:"saim", age:20, discription:"#1st YR Student"},
-        {name:"aadil", age:30, discription:"#2nd YR Student"},
-        {name:"rizwan", age:40, discription:"#3rd YR Student"},
-        {name:"mujahid", age:50, discription:"#4th YR Student"},
-        {name:"aamer", age:60, discription:"#5th YR Student"},
-        {name:"sohail", age:70, discription:"#6th YR Student"},
-        {name:"kashif", age:80, discription:"#7th YR Student"},
+        {id:"1", name:"saim", age:20, discription:"#1st YR Student"},
+        {id:"2", name:"aadil", age:30, discription:"#2nd YR Student"},
+        {id:"3", name:"rizwan", age:40, discription:"#3rd YR Student"},
+        {id:"4", name:"mujahid", age:50, discription:"#4th YR Student"},
+        {id:"5", name:"aamer", age:60, discription:"#5th YR Student"},
+        {id:"6", name:"sohail", age:70, discription:"#6th YR Student"},
+        {id:"7", name:"kashif", age:80, discription:"#7th YR Student"},
     ],
     
 };
