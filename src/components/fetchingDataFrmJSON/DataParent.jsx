27f269e8df115d71b7e8d9d93b6a732749f11623
mzhr123 <76685693+mzhr123@users.noreply.github.com>
2021-02-13 16:30:00 +0530

    saved

--- src/components/fetchingDataFrmJSON/DataParent.jsx
+++ src/components/fetchingDataFrmJSON/DataParent.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { DataChild } from "../fetchingDataFrmJSON/Datachild";
 import { personDetailsReducer } from "../State/Reducer/personDetailsReducer";
 
 export const DataParent =()=>{
-    const {personDetails, SetPersonData}=useState();
     return(
         <div>
+            
             <DataChild/>
         </div>
     );
