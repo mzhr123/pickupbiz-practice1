42a6af6e24686641b2f95a972d3998facdfc4d71
mzhr123 <76685693+mzhr123@users.noreply.github.com>
2021-02-15 16:26:56 +0530

    practice

--- src/components/fetchingDataFrmJSON/DataParent.jsx
+++ src/components/fetchingDataFrmJSON/DataParent.jsx
@@ -1,16 +1,34 @@
+// import React from "react";
+// import { DataChild } from "../fetchingDataFrmJSON/Datachild";
+// import { personDetailsReducer } from "../State/Reducer/personDetailsReducer";
+
+// export const DataParent =()=>{
+//     return(
+//         <div>
+//             {personDetailsReducer.personDetails.map((fetchData) => {
+//                 return <DataChild itemCurrent={fetchData}/>
+                
+//             })
+//             };
+
+//         </div>
+//     );
+// };
+
+
+
 import React from "react";
-import { DataChild } from "../fetchingDataFrmJSON/Datachild";
+import { Datachild } from "./Datachild";
 import { personDetailsReducer } from "../State/Reducer/personDetailsReducer";
 
 export const DataParent =()=>{
     return(
         <div>
-            {personDetailsReducer.personDetails.map((fetchData) => {
-                return <DataChild itemCurrent={fetchData}/>
-                
-            })
-            };
-
+                {personDetailsReducer.personDetails.map((fetchData) => {
+                    return <Datachild itemCurrent={fetchData}/>
+                }
+                )}            
+            
         </div>
     );
 };
--- src/components/fetchingDataFrmJSON/Datachild.jsx
+++ src/components/fetchingDataFrmJSON/Datachild.jsx
@@ -1,10 +1,24 @@
+// import React from "react";
+
+// export const DataChild =({itemCurrent})=>{
+//     return(
+//         <div>
+//             {/* <img src={props.studentimage} width="100" height="100" /> */}
+//             <p>The Student name {itemCurrent.name} is, his Age is {itemCurrent.age} and he is in Standard {itemCurrent.discription} </p>
+//         </div>
+//     );
+// };
+
+
+
+
+
 import React from "react";
 
-export const DataChild =({itemCurrent})=>{
+export const Datachild = () => {
     return(
         <div>
-            {/* <img src={props.studentimage} width="100" height="100" /> */}
-            <p>The Student name {itemCurrent.name} is, his Age is {itemCurrent.age} and he is in Standard {itemCurrent.discription} </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, nihil eos at tempore nemo aspernatur nobis omnis! Dolores similique rerum accusantium perspiciatis quidem aperiam doloremque praesentium quo adipisci, corrupti magnam.</p>
         </div>
     );
 };
