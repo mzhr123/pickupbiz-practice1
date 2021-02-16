a7ea3eebbae7a015c3deef50e6e1eb97e495d291
mzhr123 <76685693+mzhr123@users.noreply.github.com>
2021-02-16 15:56:26 +0530

    manage data

--- src/components/State/Reducer/GropuReducer.js
+++ src/components/State/Reducer/GropuReducer.js
@@ -1,3 +1,11 @@
 const GroupReducer =()=>{
-    
+
+}
+
+const groupsDefaultState ={
+    gropus:[
+        {name:"Redux Practicle",image:"Assets/image1.jpg",desc:"This Is First Group Created With Redux"},
+        {name:"Redux Developers",image:"Assets/image1.jpg",desc:"Redux Is Very Helpful For Developers"},
+        {name:"Redux sessions",image:"Assets/image1.jpg",desc:"Very Intresting Sessions"},
+    ]
 }
